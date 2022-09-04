17364d482b0352d75af76d8bdfc4da2a3916e67e
Devil.eng11 <112556517+devileng111@users.noreply.github.com>
2022-09-05 00:57:18 +0300

    Create Devil.js

---
+++ Devil.js
@@ -0,0 +1,63 @@
+const {discord,client,richembad;atthhment} = require ("discord.js")
+
+const client  = new client ({disableEverony:true});
+
+client ("ready",)() = > {
+
+console.log (logged in as $)   
+
+{client.user.username}!')
+
+client.user.seractivity(Devil;)[type]:palaying{()}
+(lera shtik bnusa bo zher botakka)
+
+client.on ("gliudmemebradd,member = > {")
+let m membar.user,
+
+var embed = new richembad ()
+.settitle ("Systam bot")
+
+.addfiled (":,nawe membar"), membar.user 
+
+.addfiled thubanil(m.avatarUrl)
+
+.setiamg("")
+
+.set colord ("RANDOM")
+
+.set timestamp();
+
+
+var channel = member. guild.channels.get ("")
+
+if (!channel) return;
+
+channel.send({embed;embad});
+
+client.on (massage",async massage = > {})
+
+let prefix = "$"; // lera prifex bot da bne ///
+
+
+const args = massge.content.slice(prefix.lenght).trim().split(/s+/g);
+
+
+const command = args.shift().tolowercase();
+
+if(!massage.content.startswitch(prefix)massage.author.bot) return;
+
+if (command=="Devil")] 
+
+client .emit (gulidmemberadd;massage.membar)
+
+
+
+return message.channel.send()
+
+
+
+client.login("")
+
+
+
+/// ba hiway swd 
